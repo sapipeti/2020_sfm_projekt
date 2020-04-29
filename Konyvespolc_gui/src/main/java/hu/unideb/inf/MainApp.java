03151e946ed5ca0c48f2c524a2037ec462cb279c
sapipeti <sapbod83@gmail.com>
2020-04-29 09:17:14 +0200

    merghez elokeszites2

--- Konyvespolc_gui/src/main/java/hu/unideb/inf/MainApp.java
+++ Konyvespolc_gui/src/main/java/hu/unideb/inf/MainApp.java
@@ -1,40 +1,6 @@
-package hu.unideb.inf;
 
-import hu.unideb.inf.view.FXMLStudentsSceneController;
-import java.util.ArrayList;
-import java.util.List;
-import javafx.application.Application;
-import static javafx.application.Application.launch;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import net.javaguides.hibernate.entity.Konyv;
-import net.javaguides.hibernate.util.JpaKonyvDAO;
+public class App extends Application {
 
-public class MainApp extends Application {
-
-    @Override
-    public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(MainApp.class.getResource("/fxml/FXMLStudentsScene.fxml"));
-        Scene scene = new Scene(loader.load());
-        stage.setTitle("Book Register_2.0");
-        stage.setScene(scene);
-        stage.show();
-        
-    }
-
-    /**
-     * 
-     * The main() method is ignored in correctly deployed JavaFX application.
-     * main() serves only as fallback in case the application can not be
-     * launched through deployment artifacts, e.g., in IDEs with limited FX
-     * support. NetBeans ignores main().
-     *
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        launch(args);
-    }
+   
 
 }
